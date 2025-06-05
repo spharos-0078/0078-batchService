918abd8ddb4177b5651783c9a92cc960fb0586ac
sujin31 <dltnwls301@gmail.com>
2025-06-05 11:53:47 +0900

    first commit

---
+++ src/main/java/com/pieceofcake/batch_service/BatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pieceofcake.batch_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchServiceApplication.class, args);
+    }
+
+}
