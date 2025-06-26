1842d17f4b5320584a6de43aac51ad003d2ed33c
sujin31 <dltnwls301@gmail.com>
2025-06-26 17:29:07 +0900

    [feature] 스웨거 연동 및 공통 응답 객체 추가 - 스웨거 추가 - 응답 객체 추가
    #9

--- src/main/java/com/pieceofcake/batch_service/chart/presentation/ChartController.java
+++ src/main/java/com/pieceofcake/batch_service/chart/presentation/ChartController.java
@@ -2,6 +2,7 @@ package com.pieceofcake.batch_service.chart.presentation;
 
 import com.pieceofcake.batch_service.chart.application.ChartEventService;
 import com.pieceofcake.batch_service.chart.dto.out.GetChartRealTimeResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ public class ChartController {
 
     private final ChartEventService chartEventService;
 
+    @Operation(summary = "실시간 체결가(시장가) 조회 SSE")
     @GetMapping(value = "/graph/real-time/{pieceProductUuid}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<GetChartRealTimeResponseDto> getChartRealTime(@PathVariable String pieceProductUuid){
         return chartEventService.getChartRealTime(pieceProductUuid);
